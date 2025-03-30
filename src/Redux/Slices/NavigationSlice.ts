b0f0aa2070b22d9008e3e752a60de416d04ee54a
Alex Harrison <alex@alexkharrison.com>
2025-03-30 11:06:15 -0400

    Remove log

---
+++ src/Redux/Slices/NavigationSlice.ts
@@ -0,0 +1,24 @@
+import { createSlice, PayloadAction } from '@reduxjs/toolkit'
+import { TAB_BREAKPOINT } from '../../Config/Device'
+
+export interface NavigationState {
+  currentRoute: string
+}
+
+const initialState: NavigationState = {
+    currentRoute: ""
+}
+
+
+const navigationSlice = createSlice({
+  name: 'navigationSlice',
+  initialState,
+  reducers: {
+    setCurrentRoute(state, action: PayloadAction<string>) {
+      state.currentRoute = action.payload
+    },
+  },
+})
+
+export const { setCurrentRoute } = navigationSlice.actions
+export default navigationSlice.reducer
